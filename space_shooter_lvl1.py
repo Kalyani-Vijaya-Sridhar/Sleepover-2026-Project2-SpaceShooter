f0a8a2387d1efd7ad3a7263620139bb812d1d811
Kalyani Vjaya Sridhar <kalyani.vijaya.sridhar@gmail.com>
2026-03-09 21:25:11 -0400

    Game starts and finishes, you can win and lose, kill the aliens or get killed by them shooting back.

--- space_shooter_lvl1.py
+++ space_shooter_lvl1.py
@@ -1,6 +1,12 @@
 import random
 import pygame
+from pygame import mixer
 from pygame.locals import *
+
+
+pygame.mixer.pre_init(44100, -16, 2, 512)
+mixer.init()
+pygame.init()
 
 # frames per second
 clock = pygame.time.Clock()
@@ -12,19 +18,45 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Space Shooter')
 
+
+# define fonts
+font30 = pygame.font.SysFont('Costantia', 30)
+font40 = pygame.font.SysFont('Costantia', 40)
+
+# load sounds
+explosion_fx = pygame.mixer.Sound('C:\\Users\\break\\OneDrive\\Home\\Programming\\Girls Who Code\\SLEEPOVER\\Space Shooter\\Images\\explosion.wav')
+explosion_fx.set_volume(0.25)
+
+explosion2_fx = pygame.mixer.Sound('C:\\Users\\break\\OneDrive\\Home\\Programming\\Girls Who Code\\SLEEPOVER\\Space Shooter\\Images\\explosion2.wav')
+explosion2_fx.set_volume(0.25)
+
+laser_fx = pygame.mixer.Sound('C:\\Users\\break\\OneDrive\\Home\\Programming\\Girls Who Code\\SLEEPOVER\\Space Shooter\\Images\\laser.wav')
+laser_fx.set_volume(0.25)
+
 # game variables
 ROWS = 4
 COLS = 5
+alien_cooldown = 1000 #milliseconds
+last_alien_shot = pygame.time.get_ticks()
+COUNTDOWN = 3
+last_count = pygame.time.get_ticks()
+GAMEOVER = 0 #0 means game is running, 1 means player won, -1 means player lost
 
 # colours
 red = (255, 0, 0)
 green = (0, 255, 0)
+white = (255, 255, 255)
 
 # background image
 bg = pygame.image.load('C:\\Users\\break\\OneDrive\\Home\\Programming\\Girls Who Code\\SLEEPOVER\\Space Shooter\\Images\\background.jpg')
 
 def draw_bg():
   screen.blit(bg, (0, 0))
+
+# function for text
+def draw_text(text, font, text_col, x, y):
+  img = font.render(text, True, text_col)
+  screen.blit(img, (x,y))
 
 # Spaceship class
 class Spaceship(pygame.sprite.Sprite):
@@ -42,6 +74,7 @@ class Spaceship(pygame.sprite.Sprite):
     speed = 8
     # cooldown time
     cooldown = 500  #milliseconds
+    GAMEOVER = 0
 
     # key press to move
     key = pygame.key.get_pressed()
@@ -55,14 +88,25 @@ class Spaceship(pygame.sprite.Sprite):
 
     # key press to shoot
     if key[pygame.K_SPACE] and time_now - self.last_shot > cooldown:
+      laser_fx.play()
       bullet = Bullets(self.rect.centerx, self.rect.top)
       bullet_group.add(bullet)
       self.last_shot = time_now
+
+    # mask
+    self.mask = pygame.mask.from_surface(self.image)
 
     # health bar
     pygame.draw.rect(screen, red, (self.rect.x, (self.rect.bottom + 10), self.rect.width, 10))
     if self.health_remaining > 0:
       pygame.draw.rect(screen, green, (self.rect.x, (self.rect.bottom + 10), int(self.rect.width * (self.health_remaining / self.health_start)), 10))
+    elif self.health_remaining <= 0:
+      explosion = Explosion(self.rect.centerx, self.rect.centery, 3)
+      explosion_group.add(explosion)
+      self.kill()
+      GAMEOVER = -1
+    return GAMEOVER
+
 
 # Bullets class
 class Bullets(pygame.sprite.Sprite):
@@ -76,6 +120,11 @@ class Bullets(pygame.sprite.Sprite):
     self.rect.y -= 5
     if self.rect.bottom < 0:
       self.kill()
+    if pygame.sprite.spritecollide(self, alien_group, True, pygame.sprite.collide_mask):
+      self.kill()
+      explosion_fx.play()
+      explosion = Explosion(self.rect.centerx, self.rect.centery, 2)
+      explosion_group.add(explosion)
 
 # Aliens class
 class Aliens(pygame.sprite.Sprite):
@@ -94,10 +143,72 @@ class Aliens(pygame.sprite.Sprite):
       self.move_direction *= -1
       self.move_counter *= self.move_direction
 
+    # mask
+    self.mask = pygame.mask.from_surface(self.image)
+
+# Alien Bullets class
+class Alien_Bullets(pygame.sprite.Sprite):
+  def __init__(self, x, y):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = pygame.image.load('C:\\Users\\break\\OneDrive\\Home\\Programming\\Girls Who Code\\SLEEPOVER\\Space Shooter\\Images\\alien_bullet.png')
+    self.rect = self.image.get_rect()
+    self.rect.center = [x, y]
+
+  def update(self):
+    self.rect.y += 2
+    if self.rect.top > screen_height:
+      self.kill()
+    if pygame.sprite.spritecollide(self, spaceship_group, False, pygame.sprite.collide_mask):
+      self.kill()
+      explosion2_fx.play()
+      # reduce spaceship health
+      spaceship.health_remaining -= 1
+      explosion = Explosion(self.rect.centerx, self.rect.centery, 1)
+      explosion_group.add(explosion)
+
+
+# Explosion Class
+class Explosion(pygame.sprite.Sprite):
+  def __init__(self, x, y, size):
+    pygame.sprite.Sprite.__init__(self)
+    self.images = []
+    for num in range(1,9):
+      img = pygame.image.load(f'C:\\Users\\break\\OneDrive\\Home\\Programming\\Girls Who Code\\SLEEPOVER\\Space Shooter\\Images\\exp{num}.png')
+      if size == 1:
+        img = pygame.transform.scale(img, (20,20))
+      if size == 2:
+        img = pygame.transform.scale(img, (40,40))
+      if size == 3:
+        img = pygame.transform.scale(img, (100,100))
+      # add the image to the list
+      self.images.append(img)
+    self.index = 0
+    self.image = self.images[self.index]
+    self.rect = self.image.get_rect()
+    self.rect.center = [x, y]
+    self.counter = 0
+
+  def update(self):
+    explosion_speed = 3
+    # explosion animation
+    self.counter += 1
+    
+    if self.counter >= explosion_speed and self.index < len(self.images) - 1:
+      self.counter = 0
+      self.index += 1
+      self.image = self.images[self.index]
+
+    # if the animation is complete delete the explosion
+    if self.index >= len(self.images) - 1 and self.counter >= explosion_speed:
+      self.kill()
+
+
 # sprite groups
 spaceship_group = pygame.sprite.Group()
 bullet_group = pygame.sprite.Group()
 alien_group = pygame.sprite.Group()
+alien_bullet_group = pygame.sprite.Group()
+explosion_group = pygame.sprite.Group()
 
 # aliens
 def create_aliens():
@@ -119,22 +230,60 @@ while run:
   # draw background
   draw_bg()
 
-  # event handlers
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      run = False
+  if COUNTDOWN == 0:
 
-  # update sprite groups
-  bullet_group.update()
-  alien_group.update()
+    # random alien bullets
+    # record current time
+    time_now = pygame.time.get_ticks()
+    # shoot
+    if time_now - last_alien_shot > alien_cooldown and len(alien_bullet_group) < 5 and len(alien_group) > 0:
+      attacking_alien = random.choice(alien_group.sprites())
+      alien_bullet = Alien_Bullets(attacking_alien.rect.centerx, attacking_alien.rect.bottom)
+      alien_bullet_group.add(alien_bullet)
+      last_alien_shot = time_now
+
+    # check if aliens are killed
+    if len(alien_group) == 0:
+      GAMEOVER = 1
+    
+    if GAMEOVER == 0:
+
+      # update spaceship
+      GAMEOVER = spaceship.update()
+
+      # update sprite groups
+      bullet_group.update()
+      alien_group.update()
+      alien_bullet_group.update()
+
+    else:
+      if GAMEOVER == -1:
+        draw_text('YOU LOST.', font40, white, int(screen_width / 2 - 85), int(screen_height / 2 + 50))
+      elif GAMEOVER == 1:  
+        draw_text('YOU WON!!!', font40, white, int(screen_width / 2 - 85), int(screen_height / 2 + 50))
+
+  if COUNTDOWN > 0:
+    draw_text('GET READY!', font40, white, int(screen_width / 2 - 85), int(screen_height / 2 + 50))
+    draw_text(str(COUNTDOWN), font40, white, int(screen_width / 2 - 8), int(screen_height / 2 + 90))
+    count_timer = pygame.time.get_ticks()
+    if count_timer - last_count > 1000:
+      COUNTDOWN -= 1
+      last_count = count_timer
+
+  # update explosion group  
+  explosion_group.update()
 
   # draw sprite groups
   spaceship_group.draw(screen)
   bullet_group.draw(screen)
   alien_group.draw(screen)
+  alien_bullet_group.draw(screen)
+  explosion_group.draw(screen)
 
-  # update spaceship
-  spaceship.update()
+  # event handlers
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
 
   pygame.display.update()
 
